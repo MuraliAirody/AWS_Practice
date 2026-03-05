e3b518955a889197547d24d63be2734a35c80eec
muraliairody <muraliairody@gmail.com>
2026-03-06 00:12:28 +0530

    ec2 demo

---
+++ EC2-demo/src/test/java/com/ec2/Ec2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
